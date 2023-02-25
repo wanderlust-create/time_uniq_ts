bf4b4c6be89b1b9f08fcee5ba7fbcbf7fb369ced
Tamara Dowis <tamara.dowis@gmail.com>
2023-02-24 18:33:21 -0600

    update function name for clarity

--- src/api/controllers/event.ts
+++ src/api/controllers/event.ts
@@ -1,16 +1,9 @@
 import * as express from "express";
 
 export default {
-<<<<<<< HEAD
   listAllGoogleCalendarEventsForDay,
 };
 
 async function listAllGoogleCalendarEventsForDay() {
-=======
-  listAllGoogleCalendrEventsForDay,
-};
-
-async function listAllGoogleCalendrEventsForDay() {
->>>>>>> a62e69ac7c1bfdbb516035208dbfb1fe73f53ebd
   console.log("listAllEvents");
 }
